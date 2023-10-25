d8d6d262cc5b0c80d3fd9a739746b6ac8e40b3f8
AtomApp23 <arsvik123@mail.ru>
2023-10-25 22:20:40 +0300

    test_remove_textview() passed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,36 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var layout: LinearLayout
+    private lateinit var textView: TextView
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        layout = findViewById(R.id.rootLayout)
+        textView = findViewById(R.id.titleTextView)
+        button = findViewById(R.id.removeButton)
+
+        button.setOnClickListener {
+            layout.removeView(textView)
+        }
+        if (savedInstanceState != null) {
+            layout.removeView(textView)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.putBoolean("key", true)
     }
 }
