80d7521f3b7c15e61e1557ee5f1137e7d57267a9
AtomApp23 <arsvik123@mail.ru>
2023-10-25 22:40:13 +0300

    test_change_text_runtime() passed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,21 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.titleTextView)
+        val button = findViewById<Button>(R.id.changeButton)
+
+        button.setOnClickListener {
+            textView.text = getString(R.string.i_am_an_android_developer)
+        }
     }
 }
