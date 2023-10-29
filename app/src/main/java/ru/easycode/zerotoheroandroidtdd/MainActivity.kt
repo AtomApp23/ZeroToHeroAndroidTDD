a6904309c872e21e89f749e10a9c29291cdc112a
AtomApp23 <arsvik123@mail.ru>
2023-10-29 22:04:46 +0300

    test_increment() passed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,11 +2,26 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private val count = Count.Base(2)
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("LOG_STATE", "onCreate called")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.countTextView)
+        val button = findViewById<Button>(R.id.incrementButton)
+
+        button.setOnClickListener {
+            val result = count.increment(textView.text.toString())
+            textView.text = result
+        }
     }
 }
 
@@ -14,15 +29,12 @@ interface Count {
     fun increment(number: String): String
     class Base(private val step: Int) : Count {
         init {
-            if(step == 0 || step == -1) {
-                throw IllegalStateException()
+            if (step < 1) {
+                throw IllegalStateException("step should be positive, but was $step")
             }
         }
 
         override fun increment(number: String): String {
-            if (step == -2) {
-                throw IllegalStateException("step should be positive, but was -2")
-            }
             return (number.toInt() + step).toString()
         }
 
